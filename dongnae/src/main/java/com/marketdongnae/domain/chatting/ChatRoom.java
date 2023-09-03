e49241869a5eefbcacf9ce42df6c7b52827eb1aa
Nohbin <nohbinmusic@gmail.com>
2023-09-03 14:05:55 +0900

    -- chatting ...

--- dongnae/src/main/java/com/marketdongnae/domain/chatting/ChatRoom.java
+++ dongnae/src/main/java/com/marketdongnae/domain/chatting/ChatRoom.java
@@ -2,25 +2,14 @@ package com.marketdongnae.domain.chatting;
 
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
 
 import lombok.Data;
 
 @Data
 public class ChatRoom {
-	private int chatroomId;
-    private int buyNumber;
-    private int gId;
+	private int chatroom_id;
+    private int buyer_M_Number;
+    private int seller_M_Number;
+    private int g_id;
     private Timestamp chatRegdate;
-    private List<MessageVO> messages = new ArrayList<>();
-
-
-    public void addMessage(MessageVO message) {
-        messages.add(message);
-    }
-
-    public List<MessageVO> getMessages() {
-        return messages;
-    }
 }

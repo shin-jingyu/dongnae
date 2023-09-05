33704d091cf2e15b695d7808301ede7db6c01a5e
hyeonjilee <skylar12200@gmail.com>
2023-09-05 17:40:53 +0900

    member

--- dongnae/src/main/java/com/marketdongnae/controller/goods/GoodsController.java
+++ dongnae/src/main/java/com/marketdongnae/controller/goods/GoodsController.java
@@ -97,8 +97,8 @@ public class GoodsController {
 	public String goods_Add_Post(@ModelAttribute GoodsDTO goodsDTO, @RequestParam("uploadFile") MultipartFile [] uploadFile) {
 
 //		String uploaderFolder = "/Users/nohbin/git/Spring_dongnaeMarket/dongnae/src/main/webapp/resources/upload/goods";
-//		String uploaderFolder = "/Users/hyeonjilee/git/dongnaeMarket/dongnae/src/main/webapp/resources/upload/goods";
-		String uploaderFolder = "C:\\Users\\jingyu\\git\\Spring_dongnaeMarket\\dongnae\\src\\main\\webapp\\resources\\upload\\goods\\";
+		String uploaderFolder = "/Users/hyeonjilee/git/dongnaeMarket/dongnae/src/main/webapp/resources/upload/goods";
+//		String uploaderFolder = "C:\\Users\\jingyu\\git\\Spring_dongnaeMarket\\dongnae\\src\\main\\webapp\\resources\\upload\\goods\\";
 
 		
 		
